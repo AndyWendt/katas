c3ba595b5f4514c99588160d093be7512519ce46
Andy Wendt <andy@awendt.com>
2018-04-18 13:06:08 -0600

    Add PrimeFactors and spec

---
+++ spec/PrimeFactorsSpec.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace spec\Katas;
+
+use Katas\PrimeFactors;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class PrimeFactorsSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(PrimeFactors::class);
+    }
+}
