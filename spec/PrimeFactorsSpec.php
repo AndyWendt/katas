a1bc0d5f47a928fd0a2545ed96b984c9cbc0eaf7
Andy Wendt <andy@awendt.com>
2019-01-26 13:42:34 -0700

    Tests passing up to 4

--- spec/PrimeFactorsSpec.php
+++ spec/PrimeFactorsSpec.php
@@ -12,4 +12,24 @@ class PrimeFactorsSpec extends ObjectBehavior
     {
         $this->shouldHaveType(PrimeFactors::class);
     }
+
+    function it_returns_an_empty_array_for_1()
+    {
+        $this->generate(1)->shouldReturn([]);
+    }
+
+    function it_should_return_2_for_2()
+    {
+        $this->generate(2)->shouldReturn([2]);
+    }
+
+    function it_should_return_3_for_3()
+    {
+        $this->generate(3)->shouldReturn([3]);
+    }
+
+    function it_should_return_2_2_for_4()
+    {
+        $this->generate(4)->shouldReturn([2,2]);
+    }
 }
--- src/PrimeFactors.php
+++ src/PrimeFactors.php
@@ -4,4 +4,54 @@ namespace Katas;
 
 class PrimeFactors
 {
+    public function generate(int $number)
+    {
+        $result = $this->calculateFactors($number);
+
+        var_dump($result);
+        return $result;
+    }
+
+    private function calculateFactors(int $number, array $factors = [])
+    {
+//        var_dump($number, $factors);
+
+        if ($number === 1) {
+            return $factors;
+        }
+
+        if ($this->numberIsPrime($number)) {
+            array_push($factors, $number);
+            return $factors;
+        }
+
+        array_push($factors, 2);
+        $remainder = $number / 2;
+
+        return $this->calculateFactors($remainder, $factors);
+    }
+
+    private function numberIsPrime(int $number): bool
+    {
+        /*
+         *  If 59 were not a prime number, then it would be divisible by at
+         * least one prime number less than or equal to √59 ≈ 7.7. Since 59
+         * cannot be divided evenly by 2, 3, 5, or 7, we know that 59 is a prime number.
+         */
+
+        /*
+         * "Prime Factorization" is finding
+         * which prime numbers multiply together to make the original number.
+         * How to find prime numbers?
+         */
+
+        /*
+         * A Prime Number can be divided exactly only by 1 or itself.
+         * And it must be a whole number greater than 1.
+         */
+
+//        sqrt($number);
+
+        return in_array($number % 2, [1]);
+    }
 }
