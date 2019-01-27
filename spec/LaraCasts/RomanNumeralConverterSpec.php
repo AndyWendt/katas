17b2882f860d2653077ae4341ab1152ffb518bb4
Andy Wendt <andy@awendt.com>
2019-01-27 12:11:08 -0700

    Add in constant map

--- spec/LaraCasts/RomanNumeralConverterSpec.php
+++ spec/LaraCasts/RomanNumeralConverterSpec.php
@@ -22,4 +22,29 @@ class RomanNumeralConverterSpec extends ObjectBehavior
     {
         $this->convert(5)->shouldReturn('V');
     }
+
+    function it_converts_10()
+    {
+        $this->convert(10)->shouldReturn('X');
+    }
+
+    function it_converts_50()
+    {
+        $this->convert(50)->shouldReturn('L');
+    }
+
+    function it_converts_100()
+    {
+        $this->convert(100)->shouldReturn('C');
+    }
+
+    function it_converts_500()
+    {
+        $this->convert(500)->shouldReturn('D');
+    }
+
+    function it_converts_1000()
+    {
+        $this->convert(1000)->shouldReturn('M');
+    }
 }
--- src/LaraCasts/RomanNumeralConverter.php
+++ src/LaraCasts/RomanNumeralConverter.php
@@ -7,6 +7,11 @@ class RomanNumeralConverter
     const NUMERAL_MAP = [
         1 => "I",
         5 => "V",
+        10 => "X",
+        50 => 'L',
+        100 => 'C',
+        500 => 'D',
+        1000 => 'M',
     ];
 
     public function convert(int $number): string
