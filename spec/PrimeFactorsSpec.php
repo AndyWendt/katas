5a947546081bc68004a3e3ccf17f1ffd52e6d4ff
Andy Wendt <andy@awendt.com>
2019-01-27 10:41:12 -0700

    Add test for 76

--- spec/PrimeFactorsSpec.php
+++ spec/PrimeFactorsSpec.php
@@ -18,33 +18,38 @@ class PrimeFactorsSpec extends ObjectBehavior
         $this->generate(1)->shouldReturn([]);
     }
 
-    function it_should_return_2_for_2()
+    function it_should_return_2_the_prime_factors_for_2()
     {
         $this->generate(2)->shouldReturn([2]);
     }
 
-    function it_should_return_3_for_3()
+    function it_should_return_the_prime_factors_for_3()
     {
         $this->generate(3)->shouldReturn([3]);
     }
 
-    function it_should_return_5_for_5()
+    function it_should_return_the_prime_factors_for_5()
     {
         $this->generate(5)->shouldReturn([5]);
     }
 
-    function it_should_return_2_2_for_4()
+    function it_should_return_the_prime_factors_for_4()
     {
         $this->generate(4)->shouldReturn([2,2]);
     }
 
-    function it_should_return_5_5_for_25()
+    function it_should_return_the_prime_factors_for_25()
     {
         $this->generate(25)->shouldReturn([5,5]);
     }
 
-    function it_should_return_2_2_2_2_5_5_5_5_for_10000()
+    function it_should_return_the_prime_factors_for_10000()
     {
         $this->generate(10000)->shouldReturn([2,2,2,2,5,5,5,5]);
+    }
+
+    function it_should_return_the_prime_factors_for_76()
+    {
+        $this->generate(76)->shouldReturn([2,2,19]);
     }
 }
