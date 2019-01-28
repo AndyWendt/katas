6846bdf9951beea2a4e1c4f9138e6905af571c9b
Andy Wendt <andy@awendt.com>
2019-01-27 22:30:40 -0700

    Add SudokuSolver

---
+++ src/Sudoku/SudokuSolver.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Katas\Sudoku;
+
+class SudokuSolver
+{
+    // https://dingo.sbs.arizona.edu/~sandiway/sudoku/
+
+    //Determine possible values
+    //winnow the available choices for cell (1,1) down to just 1 through reasoning by contradiction
+
+    public function solve($argument1)
+    {
+        // TODO: write logic here
+    }
+}
