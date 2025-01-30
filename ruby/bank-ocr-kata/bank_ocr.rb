5825ca2ae2be87b9161d4ba93698e85e843292fd
Andy Wendt <andy@awendt.com>
2025-01-29 20:53:57 -0700

    Add 0

--- ruby/bank-ocr-kata/bank_ocr.rb
+++ ruby/bank-ocr-kata/bank_ocr.rb
@@ -1,9 +1,38 @@
-class Account
-  def initialize()
+
+class Number
+  attr_reader :a
+  def initialize(a)
+    @a = a.transpose
   end
 
   def to_s
-    "000000000"
+    d = a.reduce(0) do |carry1, b|
+      carry1 += b.reduce(0) do |carry2, char|
+        case char
+        when '_'
+          add = 1
+        when '|'
+          add = 2
+        else
+          add = 0
+        end
+        carry2 += add
+      end
+      carry1
+    end
+    {
+      10 => "0"
+    }.fetch(d)
+  end
+end
+
+class Account
+  def initialize(a)
+    @a = a.transpose.each_slice(3).map { |a| Number.new(a) }.map(&:to_s)
+  end
+
+  def to_s
+    @a.join("")
   end
 end
 
@@ -13,11 +42,13 @@ class Accounts
 
   attr_reader :accounts
   def initialize(numbers_cases)
-    @accounts = [Account.new()]
+    accounts_arrays = numbers_cases.split("\n")
+                           .map(&:chars)
+                           .each_slice(4).map { |a| Account.new(a) }.map(&:to_s)
+    @accounts = accounts_arrays
   end
 
   def [](index)
     accounts[index].to_s
   end
-
 end
--- ruby/bank-ocr-kata/bank_ocr_spec.rb
+++ ruby/bank-ocr-kata/bank_ocr_spec.rb
@@ -7,6 +7,16 @@ describe "user story 1" do
     expect(first).to eq("000000000")
   end
 
+  it "parses a number" do
+    zero_array = [
+      [' ', '|', '|'],
+      ['_', ' ', '_'],
+      [' ', '|', '|'],
+    ]
+    result = Number.new(zero_array).to_s
+    expect(result).to eq("0")
+  end
+
   def numbers_cases
     File.read(File.join(__dir__, 'user-story-1-000000000.txt'))
   end
