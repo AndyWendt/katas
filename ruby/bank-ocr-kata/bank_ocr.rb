67075c4290c19d23652c9e9516a342e30edc6fbe
Andy Wendt <andy@awendt.com>
2025-02-02 16:02:44 -0700

    Green for all zeros

---
+++ ruby/bank-ocr-kata/bank_ocr.rb
@@ -0,0 +1,9 @@
+class Accounts
+  def self.from_string(account_numbers_string)
+    self.new
+  end
+
+  def to_a
+    ['000000000']
+  end
+end
--- ruby/bank-ocr-kata/bank_ocr_spec.rb
+++ ruby/bank-ocr-kata/bank_ocr_spec.rb
@@ -1,8 +1,23 @@
 require 'rspec'
 require_relative 'bank_ocr'
 
-describe "user story 1" do
-  it "parses an account number" do
+def accounts_file_reader(file_name)
+  file_path = File.join(__dir__, file_name)
+  File.open(file_path)
+end
 
+describe "user story 1" do
+  describe Accounts do
+    it "parses an account number" do
+
+      {
+        :'user-story-1-000000000.txt' => '000000000',
+      }.each do |file_name_symbol, expected|
+        account_numbers_string = accounts_file_reader(file_name_symbol.to_s)
+        result = Accounts.from_string(account_numbers_string).to_a
+
+        expect(result).to eq([expected])
+      end
+    end
   end
 end
